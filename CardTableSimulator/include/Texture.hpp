060c84fd43f2c4d4621b2307625000008d064617
Samuel Bridgham <moosethree473@gmail.com>
2023-08-13 21:28:24 -0400

    Texture class drafted

--- CardTableSimulator/include/Texture.hpp
+++ CardTableSimulator/include/Texture.hpp
@@ -1,10 +1,12 @@
 #ifndef TEXTURE_HPP
 #define TEXTURE_HPP
 
+#include "SDL2/SDL_render.h"
+
 #include <filesystem>
 
 extern "C" {
-	struct SDL_Texture;
+	struct SDL_Surface;
 }
 
 /*
@@ -16,8 +18,22 @@ class Texture {
 
 public:
 	Texture() = default;
-	Texture(const std::filesystem::path& filepath);
+	Texture(SDL_Renderer* ren, const std::filesystem::path& filepath);
+	Texture(SDL_Renderer* ren, SDL_Surface* surface);
 	~Texture();
+
+	// For now, until our requirements are more defined.
+	Texture(const Texture&) = delete;
+	Texture(Texture&&) noexcept = delete;
+	Texture& operator=(const Texture&) = delete;
+
+	void SetBlend(const SDL_BlendMode& mode);
+	void SetColor(uint8_t r, uint8_t g, uint8_t b, uint8_t a);
+
+	SDL_BlendMode GetBlend() const;
+	SDL_Color GetColor() const;
+
+	SDL_Texture* Get() const;
 
 };
 
--- CardTableSimulator/src/Texture.cpp
+++ CardTableSimulator/src/Texture.cpp
@@ -1,1 +1,47 @@
 #include "Texture.hpp"
+
+#include "SDL2/SDL_surface.h"
+#include "SDL2/SDL_image.h"
+
+Texture::Texture(SDL_Renderer* ren, const std::filesystem::path& filepath)
+	: _texture(IMG_LoadTexture(ren, filepath.string().c_str()))
+{
+}
+
+Texture::Texture(SDL_Renderer* ren, SDL_Surface* surface)
+	: _texture(SDL_CreateTextureFromSurface(ren, surface))
+{
+}
+
+Texture::~Texture()
+{
+	SDL_DestroyTexture(_texture);
+}
+
+SDL_Texture* Texture::Get() const { return _texture; }
+
+void Texture::SetBlend(const SDL_BlendMode& mode)
+{
+	SDL_SetTextureBlendMode(_texture, mode);
+}
+
+void Texture::SetColor(uint8_t r, uint8_t g, uint8_t b, uint8_t a)
+{
+	SDL_SetTextureColorMod(_texture, r, g, b);
+	SDL_SetTextureAlphaMod(_texture, a);
+}
+
+SDL_BlendMode Texture::GetBlend() const
+{
+	SDL_BlendMode m;
+	SDL_GetTextureBlendMode(_texture, &m);
+	return m;
+}
+
+SDL_Color Texture::GetColor() const
+{
+	SDL_Color c{};
+	SDL_GetTextureColorMod(_texture, &c.r, &c.b, &c.g);
+	SDL_GetTextureAlphaMod(_texture, &c.a);
+	return c;
+}
