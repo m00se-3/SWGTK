248576cd1e50444464885496022811e60605838e
Samuel Bridgham <moosethree473@gmail.com>
2024-01-20 14:01:27 -0500

    add simple constructor

--- CardTableSimulator/include/Scene.hpp
+++ CardTableSimulator/include/Scene.hpp
@@ -12,7 +12,16 @@ namespace cts
 
 	class Scene
 	{
+	public:
+		Scene(SDL_Renderer* ren);
+		~Scene() = default;
 
+
+	protected:
+
+
+	private:
+		SDL_Renderer* _renderer = nullptr;
 	};
 
 }
--- CardTableSimulator/src/Scene.cpp
+++ CardTableSimulator/src/Scene.cpp
@@ -1,6 +1,12 @@
 #include "Scene.hpp"
 
+#include "SDL.h"
+
 namespace cts
 {
+	Scene::Scene(SDL_Renderer* ren)
+		: _renderer(ren)
+	{
 
+	}
 }
