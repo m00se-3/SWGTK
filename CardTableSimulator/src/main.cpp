ba455ce78ffc26845411d5a9536299d47d18980d
Samuel Bridgham <moosethree473@gmail.com>
2024-01-31 22:06:08 -0500

    call Run() with pointer to MenuScene

--- CardTableSimulator/src/main.cpp
+++ CardTableSimulator/src/main.cpp
@@ -1,10 +1,12 @@
 ﻿#include "SDLApp.hpp"
 
+#include "scenes/MenuScene.hpp"
+
 
 int main(int argc, char** argv)
 {	
 	auto app = cts::SDLApp(argc, argv);
-	app.Run();
+	app.Run(std::make_unique<cts::MenuScene>(&app));
 
 	return 0; // This is SDL, can't forget to return 0.
 }
