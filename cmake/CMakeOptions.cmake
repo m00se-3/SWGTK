9165ae12c4ffd0b8cde78af2edfa5cf46ba0f1e5
Samuel Bridgham <moosethree473@gmail.com>
2025-07-21 13:02:02 -0400

    Update CMakeOptions.cmake with better description of SWGTK_INSTALL_FREETYPE option

--- cmake/CMakeOptions.cmake
+++ cmake/CMakeOptions.cmake
@@ -2,4 +2,4 @@
 
 option(SWGTK_BUILD_EXAMPLES "Include the example programs." ON)
 option(SWGTK_NO_CCACHE "Disable ccache support." OFF)
-option(SWGTK_INSTALL_FREETYPE "Build Freetype from source." ON)
+option(SWGTK_INSTALL_FREETYPE "Build the Freetype font library from source." ON)
