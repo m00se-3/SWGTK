94bc8da04e9e0a4f67feb09a3ac1aa765206c142
Samuel Bridgham <moosethree473@gmail.com>
2025-09-08 20:21:41 -0400

    Added more tests for Radians and Degrees, including arithmetic and comparison.

--- tests/src/MathTests.cpp
+++ tests/src/MathTests.cpp
@@ -5,9 +5,11 @@
 TEST_CASE("Angle Tests") {
   SECTION("Test type traits") {
     STATIC_REQUIRE(std::is_trivially_constructible_v<swgtk::Degrees<double>>);
+    STATIC_REQUIRE(std::is_trivially_constructible_v<swgtk::Radians<double>>);
     STATIC_REQUIRE(std::is_trivially_copyable_v<swgtk::Degrees<float>>);
     STATIC_REQUIRE(std::is_trivially_copyable_v<swgtk::Radians<float>>);
     STATIC_REQUIRE(std::is_trivially_assignable_v<swgtk::Radians<double>, swgtk::Radians<double>>);
+    STATIC_REQUIRE(std::is_trivially_assignable_v<swgtk::Degrees<double>, swgtk::Degrees<double>>);
   }
 
   SECTION("Test expected cases") {
@@ -22,6 +24,16 @@ TEST_CASE("Angle Tests") {
     STATIC_REQUIRE_FALSE(swgtk::Radians{-560.0}.value() == -560.0);
     STATIC_REQUIRE(swgtk::Radians{ std::numbers::pi * 20.0 }.value() == std::numbers::pi * 2.0);
     STATIC_REQUIRE(swgtk::Degrees{ -40000.0f }.value() == -360.0f);
+  }
+
+  SECTION("Test arithmetic") {
+    STATIC_REQUIRE((swgtk::Rads{ std::numbers::pi } + (std::numbers::pi * 0.5)) == swgtk::Rads{ std::numbers::pi * 1.5 });
+    STATIC_REQUIRE((swgtk::Degs{ 540.0 } - 36) == swgtk::Degs{ 324 });
+  }
+
+  SECTION("Test comparisons") {
+    STATIC_REQUIRE(swgtk::Degs{ 144.0 } < swgtk::Degrees{ 540.0 });
+    STATIC_REQUIRE(swgtk::Rads{ std::numbers::pi / 2 } > swgtk::Rads{ std::numbers::pi * 0.4 });
   }
 }
 // NOLINTEND(readability-magic-numbers, *-avoid-magic-numbers)
