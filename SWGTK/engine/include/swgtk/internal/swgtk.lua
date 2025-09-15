faa5229a9a5fd41fb8659e955038acd67fd1716b
Samuel Bridgham <moosethree473@gmail.com>
2025-09-14 22:27:49 -0400

    Clean up swgtk.lua

--- SWGTK/engine/include/swgtk/internal/swgtk.lua
+++ SWGTK/engine/include/swgtk/internal/swgtk.lua
@@ -1,12 +1,12 @@
 swgtk = {
-    math = {
-        pi = 3.14159265358979,
-        pi2 = 3.14159265358979 * 2,
-        RadiansToDegrees = function (self, radians)
-          return (radians / self.pi) * 180.0
-        end,
-        DegreesToRadians = function (self, degrees)
-          return (degrees / 180.0) * self.pi
-        end
-    }
+  math = {
+    pi = 3.14159265358979,
+    pi2 = 3.14159265358979 * 2,
+    RadiansToDegrees = function (self, radians)
+      return (radians / self.pi) * 180.0
+    end,
+    DegreesToRadians = function (self, degrees)
+      return (degrees / 180.0) * self.pi
+    end
+  }
 }
